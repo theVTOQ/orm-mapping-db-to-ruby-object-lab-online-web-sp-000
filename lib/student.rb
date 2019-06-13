9c95d399b966600bf6ae61f5d5074bab8a13af53
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:47:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,10 +61,10 @@ class Student
   def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = '9th'
+      WHERE grade = ?;
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
   end
@@ -72,10 +72,10 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade != '12th'
+      WHERE grade != ?;
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, 12).map do |row|
       self.new_from_db(row)
     end
   end
