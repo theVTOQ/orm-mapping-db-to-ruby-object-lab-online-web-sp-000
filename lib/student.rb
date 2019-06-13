952538332cda5af4aedecae01c1ba3ffa4866888
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:42:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,4 +57,13 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = '9th'
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
