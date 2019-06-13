0306bab6c0a9cdd4a73fe4183f86d24038bb4a24
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:40:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM songs
+      SELECT * FROM students
       WHERE name = ?
       LIMIT 1;
     SQL
