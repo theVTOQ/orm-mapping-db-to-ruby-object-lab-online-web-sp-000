bacbba2512d6e98d4147f2f51e92b71b05c5568f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:50:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,4 +79,16 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      LIMIT ?;
+    SQL
+
+    DB[:conn].execute(sql, 10, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
