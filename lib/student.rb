1ad14a83d8443b6b6a28dcb9336fbb08c47eca30
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-13 19:46:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade != '12th' 
+      WHERE grade != '12th'
     SQL
 
     DB[:conn].execute(sql).map do |row|
